726c6a482f35c8b4ba4ac038c721d85c16e7550b
leonkae <leon29kae@gmail.com>
2026-03-06 12:29:33 +0300

    connected mongo database to current backend

--- server.js
+++ server.js
@@ -11,6 +11,26 @@ const app = express()
 const hostname = process.env.HOSTNAME || localhost;
 const port = process.env.PORT || 3000;
 
+const connectDb = async () => {
+    try {
+        const uri = process.env.MONGODB_URI;
+        if (!uri) {
+            throw new Error("Check URI")
+        }
+
+        await mongoose.connect(uri, {
+            serverSelectionTimeoutMS: 5000,
+            maxPoolSize: 10,
+        })
+        console.log("Mongo Atlas connected succesfully")
+    }catch(err){
+        console.error("Mongo Atlas connection error:", err)
+        process.exit(1)
+    }   
+}
+
+connectDb();
+
 app.get('/', (req, res) => {
     res.send("This is the Cyan API");
 }) 
